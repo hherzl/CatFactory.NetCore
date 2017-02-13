52e3e054fee29aeb306655fe3b380c1195ddadf8
HH <hans.herzl@live.com>
2017-02-12 19:05:15 -0500

    Version 1.0.0-alpha-build30: Code fix

--- src/CatFactory.DotNetCore/DotNetNamingConvention.cs
+++ src/CatFactory.DotNetCore/DotNetNamingConvention.cs
@@ -12,27 +12,27 @@ namespace CatFactory.DotNetCore
 
         // todo: add logic to validate name
         public String ValidName(String name)
-            => String.Join("", name.Split(' ').Select(item => CodeNamingConvention.GetPascalCase(item)));
+            => String.Join("", name.Split(' ').Select(item => NamingConvention.GetPascalCase(item)));
 
         public String GetInterfaceName(String value)
-            => CodeNamingConvention.GetPascalCase(String.Format("I{0}", ValidName(value)));
+            => NamingConvention.GetPascalCase(String.Format("I{0}", ValidName(value)));
 
         public String GetClassName(String value)
-            => CodeNamingConvention.GetPascalCase(ValidName(value));
+            => NamingConvention.GetPascalCase(ValidName(value));
 
         public String GetFieldName(String value)
-            => String.Format("m_{0}", CodeNamingConvention.GetCamelCase(ValidName(value)));
+            => String.Format("m_{0}", NamingConvention.GetCamelCase(ValidName(value)));
 
         public String GetConstantName(String value)
-            => String.Format("{0}", CodeNamingConvention.GetCamelCase(ValidName(value)));
+            => String.Format("{0}", NamingConvention.GetCamelCase(ValidName(value)));
 
         public String GetPropertyName(String value)
-            => CodeNamingConvention.GetPascalCase(ValidName(value));
+            => NamingConvention.GetPascalCase(ValidName(value));
 
         public String GetMethodName(String value)
-            => CodeNamingConvention.GetPascalCase(ValidName(value));
+            => NamingConvention.GetPascalCase(ValidName(value));
 
         public String GetParameterName(String value)
-            => CodeNamingConvention.GetCamelCase(ValidName(value));
+            => NamingConvention.GetCamelCase(ValidName(value));
     }
 }
