34e72446885603d0d8e5994f661ce306996ee7ce
HH <hans.herzl@live.com>
2018-12-17 21:23:23 -0600

    1.0.0-beta-sun-build16 Version:
    * Core package update
    
    Quick start: https://www.codeproject.com/Tips/1164636/Scaffolding-View-Models-with-CatFactory

---
+++ CatFactory.NetCore/DatabaseExtensions.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using CatFactory.CodeFactory;
+using CatFactory.NetCore.CodeFactory;
+using CatFactory.ObjectRelationalMapping;
+using CatFactory.ObjectRelationalMapping.Programmability;
+
+namespace CatFactory.NetCore
+{
+    public static class DatabaseExtensions
+    {
+        public static ICodeNamingConvention namingConvention;
+
+        static DatabaseExtensions()
+        {
+            namingConvention = new DotNetNamingConvention();
+        }
+
+        public static string GetPropertyName(this Column column)
+        {
+            var name = column.Name;
+
+            foreach (var item in DotNetNamingConvention.invalidChars)
+                name = name.Replace(item, '_');
+
+            return namingConvention.GetPropertyName(name);
+        }
+
+        public static string GetPropertyName(this Parameter parameter)
+        {
+            var name = parameter.Name;
+
+            foreach (var item in DotNetNamingConvention.invalidChars)
+                name = name.Replace(item, '_');
+
+            return namingConvention.GetPropertyName(name);
+        }
+
+        public static string GetPropertyNameHack(this ITable table, Column column)
+        {
+            var propertyName = column.HasSameNameEnclosingType(table) ? column.GetNameForEnclosing() : column.GetPropertyName();
+
+            var regex = new Regex(@"^[0-9]+$");
+
+            if (regex.IsMatch(propertyName))
+                propertyName = string.Format("V{0}", propertyName);
+
+            return propertyName;
+        }
+
+        public static string GetPropertyNameHack(this IView view, Column column)
+        {
+            var propertyName = column.HasSameNameEnclosingType(view) ? column.GetNameForEnclosing() : column.GetPropertyName();
+
+            var regex = new Regex(@"^[0-9]+$");
+
+            if (regex.IsMatch(propertyName))
+                propertyName = string.Format("V{0}", propertyName);
+
+            return propertyName;
+        }
+
+        public static bool HasSameNameEnclosingType(this Column column, ITable table)
+            => column.Name == table.Name;
+
+        public static bool HasSameNameEnclosingType(this Column column, IView view)
+            => column.Name == view.Name;
+
+        public static string GetNameForEnclosing(this Column column)
+            => string.Format("{0}1", column.Name);
+
+        public static string ResolveDatebaseType(this Database database, Column column)
+        {
+            var map = database.DatabaseTypeMaps.FirstOrDefault(item => item.DatabaseType == column.Type);
+
+            if (map == null || map.GetClrType() == null)
+                return "object";
+
+            return map.AllowClrNullable ? string.Format("{0}?", map.GetClrType().Name) : map.GetClrType().Name;
+        }
+
+        public static DatabaseTypeMap ResolveType(this Database database, string type)
+            => database.DatabaseTypeMaps.FirstOrDefault(item => item.DatabaseType == type);
+
+        public static string ResolveDbType(this Database database, Column column)
+        {
+            var map = database.DatabaseTypeMaps.FirstOrDefault(item => item.DatabaseType == column.Type);
+
+            if (map == null || map.GetClrType() == null)
+                return "object";
+
+            return string.Format("DbType.{0}", map.DbTypeEnum);
+        }
+    }
+}
--- CatFactory.NetCore/ObjectOrientedProgramming/IDotNetClassDefinition.cs
+++ CatFactory.NetCore/ObjectOrientedProgramming/IDotNetClassDefinition.cs
@@ -4,7 +4,5 @@ namespace CatFactory.NetCore.ObjectOrientedProgramming
 {
     public interface IDotNetClassDefinition : IDotNetObjectDefinition, IClassDefinition
     {
-        // todo: Remove this flag in next core package update
-        bool IsAbstract { get; set; }
     }
 }
