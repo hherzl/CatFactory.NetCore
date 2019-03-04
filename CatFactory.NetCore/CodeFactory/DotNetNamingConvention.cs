d237d731da8b6be27a44225376a33836087b6f06
emc2 <ewmccarty@gmail.com>
2019-03-04 14:39:35 -0600

    ValidName must be outside

--- CatFactory.NetCore/CodeFactory/DotNetNamingConvention.cs
+++ CatFactory.NetCore/CodeFactory/DotNetNamingConvention.cs
@@ -23,33 +23,33 @@ namespace CatFactory.NetCore.CodeFactory
         public string ValidName(string value)
         {
             foreach (var item in invalidChars)
-                value = value.Replace(item, '_');
+                value = value?.Replace(item, '_');
 
-            return string.Join("", value.Split(' ').Select(item => NamingConvention.GetPascalCase(item)));
+            return string.Join("", value?.Split(' ')?.Select(item => NamingConvention.GetPascalCase(item)));
         }
 
         public string GetNamespace(params string[] values)
             => string.Join(".", values);
 
         public string GetInterfaceName(string value)
-            => string.Format("I{0}", NamingConvention.GetPascalCase(ValidName(value)));
+            => ValidName(string.Format("I{0}", NamingConvention.GetPascalCase(value)));
 
         public string GetClassName(string value)
-            => NamingConvention.GetPascalCase(ValidName(value));
+            => ValidName(NamingConvention.GetPascalCase(value));
 
         public string GetFieldName(string value)
-            => string.Format("m_{0}", NamingConvention.GetCamelCase(ValidName(value)));
+            => ValidName(string.Format("m_{0}", NamingConvention.GetCamelCase(value)));
 
         public string GetConstantName(string value)
-            => NamingConvention.GetCamelCase(ValidName(value));
+            => ValidName(NamingConvention.GetCamelCase(value));
 
         public string GetPropertyName(string value)
-            => NamingConvention.GetPascalCase(ValidName(value));
+            => ValidName(NamingConvention.GetPascalCase(value));
 
         public string GetMethodName(string value)
-            => NamingConvention.GetPascalCase(ValidName(value));
+            => ValidName(NamingConvention.GetPascalCase(value));
 
         public string GetParameterName(string value)
-            => NamingConvention.GetCamelCase(ValidName(value));
+            => ValidName(NamingConvention.GetCamelCase(value));
     }
 }
