97f9ce4fa0ceb68aea48c9b42c66d88baabc4ed8
emc2 <ewmccarty@gmail.com>
2019-03-05 16:29:39 -0600

    Eliminated code redundancies with DotNetNamingConvention

--- CatFactory.NetCore/CSharpProjectExtensions.cs
+++ CatFactory.NetCore/CSharpProjectExtensions.cs
@@ -8,30 +8,29 @@ namespace CatFactory.NetCore
 {
     public static class CSharpProjectExtensions
     {
-        public static string PropertyNamePattern;
-
         static CSharpProjectExtensions()
         {
-            PropertyNamePattern = @"^[0-9]+$";
         }
 
-        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project, string name) where TProjectSettings : class, IProjectSettings, new()
-        {
-            foreach (var item in DotNetNamingConvention.invalidChars)
-                name = name.Replace(item, '_');
+        public static string
+            GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project, string name)
+            where TProjectSettings : class, IProjectSettings, new() => project.CodeNamingConvention.GetPropertyName
+            (name);
 
-            var propertyName = project.CodeNamingConvention.GetPropertyName(name);
+        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project,
+            ITable table, Column column) where TProjectSettings : class, IProjectSettings, new()
+            => table.Name == column.Name
+                ? $"{project.GetPropertyName(column.Name)}1"
+                : project.GetPropertyName(column.Name);
 
-            return new Regex(PropertyNamePattern).IsMatch(propertyName) ? string.Format("V{0}", propertyName) : propertyName;
-        }
+        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project, IView view,
+            Column column) where TProjectSettings : class, IProjectSettings, new()
+            => view.Name == column.Name
+                ? $"{project.GetPropertyName(column.Name)}1"
+                : project.GetPropertyName(column.Name);
 
-        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project, ITable table, Column column) where TProjectSettings : class, IProjectSettings, new()
-            => table.Name == column.Name ? string.Format("{0}1", project.GetPropertyName(column.Name)) : project.GetPropertyName(column.Name);
-
-        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project, IView view, Column column) where TProjectSettings : class, IProjectSettings, new()
-            => view.Name == column.Name ? string.Format("{0}1", project.GetPropertyName(column.Name)) : project.GetPropertyName(column.Name);
-
-        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project, Parameter parameter) where TProjectSettings : class, IProjectSettings, new()
+        public static string GetPropertyName<TProjectSettings>(this CSharpProject<TProjectSettings> project,
+            Parameter parameter) where TProjectSettings : class, IProjectSettings, new()
             => project.GetPropertyName(parameter.Name);
     }
 }
