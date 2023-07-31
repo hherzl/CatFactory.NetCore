1428071be48a4ffc570c77679be3194896418a2b
HH <hans.herzl@live.com>
2023-07-31 02:21:38 -0600

    1.0.0-beta-sun-build45 Version:
          Fix issue 16 https://github.com/hherzl/CatFactory.NetCore/issues/16
          Fix issue 17 https://github.com/hherzl/CatFactory.NetCore/issues/17
          Fix issue 18 https://github.com/hherzl/CatFactory.NetCore/issues/18
          Fix issue 19 https://github.com/hherzl/CatFactory.NetCore/issues/19
    
          Quick start: https://www.codeproject.com/Tips/1164636/Scaffolding-View-Models-with-CatFactory

--- CatFactory.NetCore/ObjectOrientedProgramming/ClassConstructorDefinitionExtensions.cs
+++ CatFactory.NetCore/ObjectOrientedProgramming/ClassConstructorDefinitionExtensions.cs
@@ -19,9 +19,30 @@ namespace CatFactory.NetCore.ObjectOrientedProgramming
             return definition;
         }
 
-        public static ClassConstructorDefinition AddLine(this ClassConstructorDefinition definition, string code)
+        public static ClassConstructorDefinition AddLine(this ClassConstructorDefinition definition, string line)
         {
-            definition.Lines.Add(new CodeLine(code));
+            definition.Lines.Add(new CodeLine(line));
+
+            return definition;
+        }
+
+        public static ClassConstructorDefinition AddEmpty(this ClassConstructorDefinition definition)
+        {
+            definition.Lines.Add(new EmptyLine());
+
+            return definition;
+        }
+
+        public static ClassConstructorDefinition AddComment(this ClassConstructorDefinition definition, string comment)
+        {
+            definition.Lines.Add(new CommentLine(comment));
+
+            return definition;
+        }
+
+        public static ClassConstructorDefinition AddTodo(this ClassConstructorDefinition definition, string todo)
+        {
+            definition.Lines.Add(new TodoLine(todo));
 
             return definition;
         }
